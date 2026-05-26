26e7a4b78e6bf056132258bb4ff1fe72a2ec5447
tupencauy83 <tupencauy83@gmail.com>
2026-05-25 23:01:10 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,12 @@ echo "Esperando SQL Server..."
 
 for i in {1..60}
 do
-    /opt/mssql-tools18/bin/sqlcmd -S localhost -U sa -P "$MSSQL_SA_PASSWORD" -No -Q "SELECT 1" > /dev/null 2>&1
+    /opt/mssql-tools18/bin/sqlcmd \
+        -S localhost \
+        -U sa \
+        -P "$MSSQL_SA_PASSWORD" \
+        -No \
+        -Q "SELECT 1" > /dev/null 2>&1
 
     if [ $? -eq 0 ]
     then
